88e2ade93e0eaae682a84b83444aac0106edd937
Arlan Gustavo Biati <arlan@MacBook-Air-de-Arlan.local>
2022-11-02 06:58:57 -0300

    :sparkles: feat: estrutura do banco e relacionamentos

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,26 +3,25 @@ import cors from '@fastify/cors';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient({
-  log: ['query']
-})
+  log: ['query'],
+});
 
 async function bootstrap() {
   const fastify = Fastify({
-    logger: true
-  })
+    logger: true,
+  });
 
   await fastify.register(cors, {
-    origin: true
-  })
+    origin: true,
+  });
 
   fastify.get('/pools/count', async () => {
-
     const count = await prisma.pool.count();
 
-    return { count }
-  })
+    return { count };
+  });
 
-  await fastify.listen({ port: 3333, /*host: '0.0.0.0'*/ })
+  await fastify.listen({ port: 3333 /*host: '0.0.0.0'*/ });
 }
 
-bootstrap()
+bootstrap();
