24cca8ba63747b19d01a5ecab918009f95f1b753
Arlan Gustavo Biati <arlan@MacBook-Air-de-Arlan.local>
2022-11-05 15:58:55 -0300

    :sparkles: feat: jwt

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,8 +1,19 @@
 import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
 import { prisma } from '../lib/prisma';
+import { authenticate } from '../plugins/authenticate';
 
 export async function authRoutes(fastify: FastifyInstance) {
+  fastify.get(
+    '/me',
+    {
+      onRequest: [authenticate],
+    },
+    async (req) => {
+      return { user: req.user };
+    }
+  );
+
   fastify.post('/users', async (req) => {
     const createUserBody = z.object({
       access_token: z.string(),
